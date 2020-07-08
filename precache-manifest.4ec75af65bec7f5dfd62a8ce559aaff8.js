835dcca31af946a3ac1ca6013c4efff15e58fc04
Paras Patidar <patidarparas13@gmail.com>
2020-07-08 21:11:25 +0530

    Updates

---
+++ precache-manifest.4ec75af65bec7f5dfd62a8ce559aaff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c829c96bebc0f7772d96746b2b94221",
+    "url": "/GithubCardGenerator/index.html"
+  },
+  {
+    "revision": "3933ec768241221bf50a",
+    "url": "/GithubCardGenerator/static/css/main.42d9750f.chunk.css"
+  },
+  {
+    "revision": "752485ecc6c2854d0842",
+    "url": "/GithubCardGenerator/static/js/2.77e0c8a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GithubCardGenerator/static/js/2.77e0c8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3933ec768241221bf50a",
+    "url": "/GithubCardGenerator/static/js/main.3c5c9bd2.chunk.js"
+  },
+  {
+    "revision": "a9614fb02c2c6e74bc5c",
+    "url": "/GithubCardGenerator/static/js/runtime-main.0451f023.js"
+  }
+]);
